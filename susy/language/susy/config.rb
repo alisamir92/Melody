a2dd5114aac429d4a2fefd3657f67948dfdab799
ali <alisamir@start-solutions.net>
2018-02-21 16:53:10 +0200

    make home-page.html and add layout and style to it

---
+++ susy/language/susy/config.rb
@@ -0,0 +1,11 @@
+# Compass CSS framework config file
+require "susy";
+
+project_type = :stand_alone
+http_path = "/"
+sass_dir = "scss"
+css_dir = "css"
+line_comments = false
+preferred_syntax = :scss
+output_style = :nested
+relative_assets = true
